e066f8bfb4ada8b22e2b8477a849cfb967ed2437
Takashi-1027 <taka19921027@gmail.com>
2021-03-04 07:12:46 +0000

    [コメント機能]ルーティングを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show,:index,:edit,:update]
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
     # View-bookの中にfavoritesを作るからresouces :booksの中に入れている。
   end
 end
