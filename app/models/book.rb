fba3eef441b45293342b7a61716d2e807479c0e3
Takashi-1027 <taka19921027@gmail.com>
2021-03-04 00:59:39 +0000

    [「いいね」モデル作成]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,9 @@
 class Book < ApplicationRecord
 	# has_many :user
 	belongs_to :user
-	
+
+	has_many :favorites, dependent: :destroy
+
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
 end
