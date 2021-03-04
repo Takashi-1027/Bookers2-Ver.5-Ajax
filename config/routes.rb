81504ace3c90864853492c5be3781403df89a438
Takashi-1027 <taka19921027@gmail.com>
2021-03-04 01:22:15 +0000

    [いいね機能]のコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    # View-bookの中にfavoritesを作るからresouces :booksの中に入れている。
+  end
 
   root to: 'homes#top'
 
 end
+
+  # get 'favorites/create'
+  # get 'favorites/destroy'
