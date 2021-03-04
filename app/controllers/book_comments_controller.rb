0de491af6f20ea774772e50bd126c1958651514d
Takashi-1027 <taka19921027@gmail.com>
2021-03-04 12:00:55 +0000

    [Finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -13,7 +13,7 @@ class BookCommentsController < ApplicationController
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(book)
+    redirect_back(fallback_location: root_path)
   end
 end
 
