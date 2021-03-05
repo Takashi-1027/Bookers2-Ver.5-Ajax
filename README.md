e35d2fe4d04566d95e86633610a4823b48d079d9
Takashi-1027 <taka19921027@gmail.com>
2021-03-05 04:13:13 +0000

    [[Finish]いいね機能と見た目の修正2回目]

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-課題3【Bookers2へ「いいね/コメント機能」を実装】に取り組む
+課題4【Bookers2へ「フォロー/フォロワー機能」を実装】に取り組む
 
-Bookers2-Ver.2のファイルに入っている
+Bookers2-Ver.3のファイルに入っている
 
 # README
 
@@ -24,15 +24,3 @@ Things you may want to cover:
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
-
-* ...
-*
-
-[28. 本の更新失敗時にもエラー文が表示されない]　18. と同様にedit.html.erbにエラー文を書き足す必要があります。
-
-上記の修正はバリデーションが作動し、エラー文が表示されているので、何もしていない。
-
-2021/03/03
-新規ユーザー登録時に・introduction can't be blankというエラーが発生した。
-解決策はmodels/user.rbのvalidates :introduction, presence: trueが影響していたみたいで
-これを削除したら解決した。
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get 'home/about' => 'homes#about'
   # root to: 'homes#top'
-
-  resources :users, only: [:show,:index,:edit,:update]
-
-  resources :books,only: [:index, :show, :edit, :create, :destroy, :update]do
+  resources :books,only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
     # View-bookの中にfavoritesを作るからresouces :booksの中に入れている。
   end
+  resources :users, only: [:show,:index,:edit,:update]
 end
   # get 'book_comments/create'
   # get 'book_comments/destroy'
