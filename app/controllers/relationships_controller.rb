95485ff8b32484d37e01a0df503a5e154c4975e0
Takashi-1027 <taka19921027@gmail.com>
2021-03-05 09:31:55 +0000

    followボタンを作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,13 @@ class RelationshipsController < ApplicationController
     relationships = Relationship.new(follower_user: current_user)
     relationships.followed = User.find(params[:id])
     relationships.save
-
+    redirect_to request.referer
   end
 
   def destroy
     relationships = Relationship.find_by(followed_user: params[:id], follower_user: current_user)
     relationships.destroy
+    redirect_to request.referer
   end
 
 end
