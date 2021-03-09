08db9c701485b84b120472e0d871cd303b9dd050
Takashi-1027 <taka19921027@gmail.com>
2021-03-09 13:34:18 +0000

    いいね機能の不具合を質問して解決した

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,21 +3,20 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @class = ".favorit-btn-#{@book.id}"
-    # favorite = @book.favorites.new(user_id: current_user.id)
-    favorite = current_user.favorites.new(book_id: @book.id)
+    # @class = ".favorit-btn-#{@book.id}"
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
     # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    @class = ".favorit-btn-#{@book.id}"
-    # favorite = @book.favorites.find_by(user_id: current_user.id)
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    # @class = ".favorit-btn-#{@book.id}"
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    # p favorite => pはfavorite内の要素を確認することができる。
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
     # redirect_back(fallback_location: root_path)
   end
 end
